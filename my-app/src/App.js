997c3b7f4957d678379671f00089d8edb32f3d2a
christophe <christophebuffe@gmail.com>
2022-09-19 20:20:10 +0200

    test problem url

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router';
 import Home from './pages/Home';
 import Game from './pages/Game';
+import Error404 from './pages/Error404';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/game" element={<Game />} />
+      <Route path="*" element={<Error404 />} />
     </Routes>
   );
 }
